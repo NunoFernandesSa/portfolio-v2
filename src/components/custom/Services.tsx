7cea4c2b3f4bd16bc642e1ba8fb14c6eab08162a
NunoF <n.fernandes.contact@gmail.com>
2025-09-07 06:08:54 +0200

    feat(Services): add subtle rotation effects on hover for service cards
    Enhance visual feedback by applying different rotation directions to service cards when hovered, making the interaction more engaging.

--- src/components/custom/Services.tsx
+++ src/components/custom/Services.tsx
@@ -31,7 +31,17 @@ From concept to long-term support, let's make your digital vision a reality."
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-center text-center gap-6">
         {SERVICES.map((service, index) => (
           <div key={index}>
-            <Card className="bg-transparent border-2 border-purple-500/10 min-h-[360px] shadow-lg shadow-purple-500/30 hover:shadow-xl hover:shadow-purple-500/50 transition-shadow duration-300">
+            <Card
+              className={`bg-transparent border-2 border-purple-500/10 min-h-[360px] shadow-lg shadow-purple-500/30 hover:shadow-xl hover:shadow-purple-500/50 hover:scale-101 ${
+                index === 0
+                  ? "hover:rotate-1"
+                  : index === 1
+                  ? "hover:-rotate-1"
+                  : index === 2
+                  ? "hover:rotate-1"
+                  : ""
+              } transition-all duration-300`}
+            >
               <CardHeader className="p-2 sm:p-6 gap-6">
                 <Image
                   src={service.icon}
