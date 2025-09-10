c7a96a6114adb1f5ad32cb1d403126b918ac26f8
NunoF <n.fernandes.contact@gmail.com>
2025-09-10 04:21:55 +0200

    feact: impplements contact component
    - add phone and email static

--- src/components/custom/Contact.tsx
+++ src/components/custom/Contact.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+// ----- icons -----
+import { IoIosPhonePortrait } from "react-icons/io";
+import { MdOutlineMarkEmailUnread } from "react-icons/md";
 
 // ----- Custom components -----
 import Section from "./Section";
 import TitleSection from "./TitleSection";
 import SubtitleSection from "./SubtitleSection";
+// ----- Shadcn components -----
+import { Card } from "../ui/shadcn/card";
 
 export default function Contact() {
   return (
@@ -11,7 +16,19 @@ export default function Contact() {
       <TitleSection title="Get in touch !" />
       <SubtitleSection text="Got a question or a project in mind? Let’s talk !" />
 
-      <div className=""></div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8 w-full">
+        <div className="">
+          <Card className="w-[300px] flex flex-row items-center justify-center gap-4 bg-transparent hover:scale-101 cursor-pointer border-purple-500/10 shadow-lg shadow-purple-500/30 hover:shadow-xl hover:shadow-purple-500/50 transition-shadow duration-300">
+            <IoIosPhonePortrait size={30} />
+            <span> +33 7 71 80 29 21</span>
+          </Card>
+          <Card className="w-[300px] flex flex-row items-center justify-center gap-4 bg-transparent hover:scale-101 cursor-pointer border-purple-500/10 shadow-lg shadow-purple-500/30 hover:shadow-xl hover:shadow-purple-500/50 transition-shadow duration-300">
+            <MdOutlineMarkEmailUnread size={30} />
+            <span>n.fernandes.contact@gmail.com</span>
+          </Card>
+        </div>
+        <div className=""></div>
+      </div>
     </Section>
   );
 }
