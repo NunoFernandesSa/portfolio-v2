a1fd4699d073442a61842378fe5a8ea6c3678ec6
NunoF <n.fernandes.contact@gmail.com>
2025-09-29 00:22:13 +0200

    feat(Projects): add project titles below drawer items
    Add project titles below each project item in the Projects component to improve visibility and user experience

--- src/components/custom/Projects.tsx
+++ src/components/custom/Projects.tsx
@@ -45,60 +45,65 @@ export default function Projects() {
 
       <div className="flex flex-wrap justify-center items-center gap-10 lg:gap-20">
         {PORTFOLIO_ITEMS.map((project, index) => (
-          <Drawer
-            key={index}
-            open={openDrawerIndex === index}
-            onOpenChange={(open) => setOpenDrawerIndex(open ? index : null)}
-          >
-            <DrawerTrigger>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Image
-                    src={project.image}
-                    alt={tp(`${project.translationKey}.title`)}
-                    width={500}
-                    height={500}
-                    className="rounded hover:scale-101 cursor-pointer border-purple-500/10 shadow-lg shadow-purple-500/30 hover:shadow-xl hover:shadow-purple-500/50 transition-shadow duration-300"
-                  />
-                </TooltipTrigger>
-                <TooltipContent>
-                  <span>{tp(`${project.translationKey}.title`)}</span>
-                </TooltipContent>
-              </Tooltip>
-            </DrawerTrigger>
-            <DrawerContent>
-              <DrawerHeader className="mx-auto">
-                <DrawerTitle>
-                  {tp(`${project.translationKey}.title`)}
-                </DrawerTitle>
-                <DrawerDescription className="max-w-2xl mx-auto">
-                  {tp(`${project.translationKey}.desc`)}
-                </DrawerDescription>
-                <div className="flex items-center justify-center gap-2">
-                  {project.technos.map((item, index) => (
-                    <span
-                      key={index}
-                      className="text-[10px]  bg-[#7420e8] px-2 py-[0.5px] rounded-full"
-                    >
-                      {item}
-                    </span>
-                  ))}
-                </div>
-              </DrawerHeader>
-              <DrawerFooter className="flex flex-row justify-center flex-wrap">
-                <Button className="w-38 cursor-pointer" asChild>
-                  <Link href={project.link} target="_blank">
-                    {t("seeProjectBtn")} <FaRegEye />
-                  </Link>
-                </Button>
-                <DrawerClose asChild>
-                  <Button variant="outline" className="w-38 cursor-pointer">
-                    {t("closeBtn")} <VscClose />
+          <div className="flex flex-col">
+            <Drawer
+              key={index}
+              open={openDrawerIndex === index}
+              onOpenChange={(open) => setOpenDrawerIndex(open ? index : null)}
+            >
+              <DrawerTrigger>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Image
+                      src={project.image}
+                      alt={tp(`${project.translationKey}.title`)}
+                      width={500}
+                      height={500}
+                      className="rounded hover:scale-101 cursor-pointer border-purple-500/10 shadow-lg shadow-purple-500/30 hover:shadow-xl hover:shadow-purple-500/50 transition-shadow duration-300"
+                    />
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <span>{tp(`${project.translationKey}.title`)}</span>
+                  </TooltipContent>
+                </Tooltip>
+              </DrawerTrigger>
+              <DrawerContent>
+                <DrawerHeader className="mx-auto">
+                  <DrawerTitle>
+                    {tp(`${project.translationKey}.title`)}
+                  </DrawerTitle>
+                  <DrawerDescription className="max-w-2xl mx-auto">
+                    {tp(`${project.translationKey}.desc`)}
+                  </DrawerDescription>
+                  <div className="flex items-center justify-center gap-2">
+                    {project.technos.map((item, index) => (
+                      <span
+                        key={index}
+                        className="text-[10px]  bg-[#7420e8] px-2 py-[0.5px] rounded-full"
+                      >
+                        {item}
+                      </span>
+                    ))}
+                  </div>
+                </DrawerHeader>
+                <DrawerFooter className="flex flex-row justify-center flex-wrap">
+                  <Button className="w-38 cursor-pointer" asChild>
+                    <Link href={project.link} target="_blank">
+                      {t("seeProjectBtn")} <FaRegEye />
+                    </Link>
                   </Button>
-                </DrawerClose>
-              </DrawerFooter>
-            </DrawerContent>
-          </Drawer>
+                  <DrawerClose asChild>
+                    <Button variant="outline" className="w-38 cursor-pointer">
+                      {t("closeBtn")} <VscClose />
+                    </Button>
+                  </DrawerClose>
+                </DrawerFooter>
+              </DrawerContent>
+            </Drawer>
+            <span className="mt-3 font-bold">
+              {tp(`${project.translationKey}.title`)}
+            </span>
+          </div>
         ))}
       </div>
     </Section>
