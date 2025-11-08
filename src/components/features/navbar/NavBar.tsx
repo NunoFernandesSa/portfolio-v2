a8d29596516f477e86e63e4b031ebc0110f0da8d
NunoF <n.fernandes.contact@gmail.com>
2025-11-08 02:49:19 +0100

    refactor(navbar): improve styling and remove unused theme toggle component
    - Update navbar styling with hover effects and better spacing
    - Remove deprecated ThemeToggle component as it's no longer needed

--- src/components/features/navbar/NavBar.tsx
+++ src/components/features/navbar/NavBar.tsx
@@ -9,15 +9,20 @@ export default function NavBar() {
   const t = useTranslations("HomePage");
 
   return (
-    <nav className="fixed top-0 left-0 right-0 h-24 flex flex-row items-center justify-center text-primary-foreground border-b border-primary-foreground bg-[#030712]">
+    <nav className="fixed top-0 left-0 right-0 h-24 flex flex-row items-center justify-center text-primary-foreground bg-primary">
       <ul className="flex flex-row items-center justify-center">
         {navItems.map((item) => (
-          <li key={item.href} className="px-4 py-2">
-            <a href={item.href}>{t("navbar" + item.label)}</a>
+          <li
+            key={item.href}
+            className="px-4 py-2 hover:bg-primary-foreground hover:text-primary ease-in-out duration-300"
+          >
+            <a href={`#${item.href}`}>
+              <p className="text-md">{t("navbar" + item.label)}</p>
+            </a>
           </li>
         ))}
 
-        <div className=" flex items-center justify-center gap-4">
+        <div className=" flex items-center justify-center gap-4 ms-3">
           <LocalSwitcher />
           <ThemeToggle />
         </div>
